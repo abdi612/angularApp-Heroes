70b2b453be4f09f670990df4347343ebbb5da521
Abdi Ali <abdialidev@gmail.com>
2021-09-11 23:39:58 -0500

    added here detail component

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -12,12 +12,13 @@ import { HEROES } from '../mock-heroes';
 export class HeroesComponent 
   implements OnInit {
     hero = HEROES
+    selectedHero?: Hero ;
   constructor() { }
 
   ngOnInit(): void {
 
   }
-  selectedHero?: Hero ;
+
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
